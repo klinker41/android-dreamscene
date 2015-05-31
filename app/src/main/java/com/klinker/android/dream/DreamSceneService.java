826bd05f99164d10ddd4662ad0e4b8de262b6af6
Jake Klinker <jklinker1@gmail.com>
2015-05-30 21:33:16 -0500

    start setting up for downloading, caching and displaying images

--- app/src/main/java/com/klinker/android/dream/DreamSceneService.java
+++ app/src/main/java/com/klinker/android/dream/DreamSceneService.java
@@ -17,8 +17,44 @@
 package com.klinker.android.dream;
 
 import android.service.dreams.DreamService;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+import java.util.Random;
 
 public class DreamSceneService extends DreamService {
+
+    private static final String TAG = "DreamSceneService";
+
+    private static final String[] BACKGROUNDS = new String[] {
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/390823.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/456548.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/456965.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/458532.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/463395.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/468561.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/468569.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/475841.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/475843.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/476288.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/478066.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/479128.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/480730.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/484717.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/491050.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/546077.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/550965.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/558952.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/562771.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/568653.jpg",
+            "https://raw.githubusercontent.com/klinker41/android-dreamscene/master/backgrounds/569143.jpg"
+    };
+
+    private ImageSwitcher background;
 
     @Override
     public void onAttachedToWindow() {
@@ -28,6 +64,29 @@ public class DreamSceneService extends DreamService {
         setInteractive(false);
         setFullscreen(true);
         setScreenBright(true);
+
+        // show the view on the screen
+        setContentView(R.layout.daydream_service);
+
+        // set up the background factory
+        background = (ImageSwitcher) findViewById(R.id.imageSwitcher);
+        background.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                return new ImageView(getApplicationContext());
+            }
+        });
+
+        Animation in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+        Animation out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
+        background.setInAnimation(in);
+        background.setOutAnimation(out);
+    }
+
+    private int getRandomBackgroundResource() {
+        Random r = new Random();
+        int num = r.nextInt(BACKGROUNDS.length);
+        return getResources().getIdentifier("back" + num, "drawable", getPackageName());
     }
 
 }
